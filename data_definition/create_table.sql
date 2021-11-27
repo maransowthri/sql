456e89623000f932b602972e4a06026920a676d0
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-11-27 19:47:25 +0530

    Created all the required tables

--- data_definition/create_table.sql
+++ data_definition/create_table.sql
@@ -13,3 +13,20 @@ create table users (
 	yearly_salary int,
 	current_status employment_status
 )
+
+-- For both MySQL and PostgreSQL
+create table employers (
+	company_name varchar(100),
+	company_address varchar(100),
+	-- yearly_revenue float(), -- Apprximation
+	yearly_revenue numeric(5, 2), -- Exactness; Allowed: 123.24; Not Allowed: 1234.343
+	is_hiring boolean
+)
+
+-- For both MySQL and PostgreSQL
+create table conversations (
+	user_name varchar(200),
+	employer_name varchar(250),
+	message text,
+	date_sent timestamp
+)
