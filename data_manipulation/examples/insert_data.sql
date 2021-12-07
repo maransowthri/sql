f6a37b35de43172918b140669fb96818fb0537df
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-07 18:11:44 +0530

    Started working on data manipulation module

--- data_manipulation/examples/insert_data.sql
+++ data_manipulation/examples/insert_data.sql
@@ -7,5 +7,4 @@ insert into sales
 (date_fulfilled, customer_name, product_name, volume, is_recurring, is_disputed)
 values
 (null, 'Learning Inc', 'Course Bundle', 4889.45, false, false),
-('2022-04-10', 'Big Oil Inc', 'Trucks', 4000.45, false, true),
-;
+('2022-04-10', 'Big Oil Inc', 'Trucks', 4000.45, false, true);
