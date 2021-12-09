fce07ddb787b2b95c4bbe2b82d54b50253828d55
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-09 17:04:55 +0530

    Finished data manipulation module

--- data_manipulation/examples/select_data.sql
+++ data_manipulation/examples/select_data.sql
@@ -11,3 +11,15 @@ select * from sales where date_created > '2021-11-01' and date_created < '2022-0
 -- Or
 
 select * from sales where date_created between '2021-11-01' and '2022-05-01';
+
+-- This just ignores Company A, Company B & Company C
+select * from sales where customer_name > 'Company C';
+
+select * from sales where extract(day from date_fulfilled - date_created) <= 5;
+
+select * from sales order by volume desc offset 6;
+
+select distinct customer_name from sales;
+
+-- 2nd most highest volume
+select volume from sales where volume <> (select volume from sales order by volume desc limit 1) order by volume desc limit 1;
