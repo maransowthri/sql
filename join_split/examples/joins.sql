730dce9569fec9ad7e7435b8f4daa73ff3752c3a
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-12 16:13:20 +0530

    Learned left joins

--- join_split/examples/joins.sql
+++ join_split/examples/joins.sql
@@ -1,5 +1,11 @@
+-- inner join - joining only common columns
 select u.first_name, a.street, c.name as city_name from users as u 
 inner join address as a on a.id=u.address_id 
 inner join city as c on c.id=a.city_id
 where u.id=1 or u.id=2
 order by u.id desc;
+
+-- left join - merging all the common data from right table to left
+select c.name, u.first_name, a.street from city as c
+left join address as a on c.id=a.city_id
+left join users as u on u.address_id=a.id;
