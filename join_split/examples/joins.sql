c3ebf596082491cdefa0a14643fd389e7981b26e
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-14 18:56:31 +0530

    Finished learning left and cross joins

--- join_split/examples/joins.sql
+++ join_split/examples/joins.sql
@@ -5,7 +5,15 @@ inner join city as c on c.id=a.city_id
 where u.id=1 or u.id=2
 order by u.id desc;
 
+
 -- left join - merging all the common data from right table to left
 select c.name, u.first_name, a.street from city as c
 left join address as a on c.id=a.city_id
 left join users as u on u.address_id=a.id;
+
+select c.name as city_name, u.first_name from city as c
+left join address as a on c.id=a.city_id
+left join users as u on a.id=u.address_id;
+
+-- Cross join - combinations of all the entries (can't use 'on' condition here)
+select * from users cross join address;
