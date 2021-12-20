0d8b5029d1c6d2a5f1d2bfe9ec3380b6332b5d80
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2021-12-20 18:06:00 +0530

    Finished joins module

---
+++ join_split/challenges/on-delete-checks.sql
@@ -0,0 +1,3 @@
+delete from building where id=3;
+delete from team where id=2;
+delete from employee where id = 1;
--- join_split/challenges/select-data.sql
+++ join_split/challenges/select-data.sql
@@ -2,3 +2,14 @@
 select e.first_name, e.last_name, p.title from employee as e 
 left join project_employee as pe on pe.employee_id=e.id 
 left join project as p on p.id=pe.project_id;
+
+-- 2. List all employees who are part of a specifc team
+select e.first_name, e.last_name, t.name from employee as e 
+inner join team as t on e.team_id=t.id 
+where t.name='Data Analysts';
+
+-- 3. Which employees are at building no 3
+select e.first_name, e.last_name, b.name from employee as e
+inner join team as t on e.team_id=t.id 
+inner join building as b on b.id=t.building_id
+where b.id=3;
