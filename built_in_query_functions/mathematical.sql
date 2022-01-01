ccdb70940dad19d9998799c95b93f8bd5f8c9cb3
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-01-01 17:30:59 +0530

    Started query functions module

---
+++ built_in_query_functions/mathematical.sql
@@ -0,0 +1,6 @@
+-- 1.Arithematics
+select price * billing_frequency as revenue from memberships;
+
+-- 2. Functions
+-- CEIL, FLOOR, ROUND, TRUNC or TRUNCATE (just cuts off the decimal places no matter what the value is)
+select consumption from memberships;
