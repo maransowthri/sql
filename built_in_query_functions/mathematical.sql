9dd45aa36cc0e7f731ddfa3a1cd4dec4b2406c8b
Maran Sowthri Kalailingam <kmaran@athenahealth.com>
2022-01-08 15:24:30 +0530

    Played with date functions

--- built_in_query_functions/mathematical.sql
+++ built_in_query_functions/mathematical.sql
@@ -4,3 +4,32 @@ select price * billing_frequency as revenue from memberships;
 -- 2. Functions
 -- CEIL, FLOOR, ROUND, TRUNC or TRUNCATE (just cuts off the decimal places no matter what the value is)
 select consumption from memberships;
+
+-- 3. Concat
+select concat(first_name, ' ', last_name) as Name from memberships;
+select concat('$ ', price) as Price from memberships;
+
+-- 4. Upper / lower case
+select upper('Maran');
+select lower('Maran');
+
+-- 5. Finding length
+select length('Maran');
+
+-- 6. Trimming a string
+select trim('    Maran     ');
+select trim(leading ' ' from  '    Maran     ');
+
+-- 7. Extracting month, day, day of the week from date
+select extract(day from last_checkin), last_checkin from memberships;
+select extract(month from last_checkin), last_checkin from memberships;
+-- For Postgres
+select extract(dow from last_checkin), last_checkin from memberships;
+-- For MySQL
+select weekday(last_checkin) from memberships;
+
+-- 8. Convert time stamps into date or time
+-- For MySQL
+select convert(last_checkin, date), convert(last_checkin, time) from memberships;
+-- For Postgres
+select last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME from memberships;
